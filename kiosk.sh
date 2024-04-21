ecec2e08182a28fcfd71971ed43fed84d3bbc767
Yuu528 <yuu528mail@gmail.com>
2024-04-21 11:28:22 +0900

    Add kiosk startup script

---
+++ kiosk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function on_exit() {
+	kill $pid
+}
+
+cd $(dirname $0)
+
+trap on_exit EXIT
+
+while true; do
+	python ./src/main.py &
+
+	pid=$!
+
+	inotifywait -e modify --exclude '.*\.swp' ./src
+
+	kill $pid
+	wait $pid
+done
